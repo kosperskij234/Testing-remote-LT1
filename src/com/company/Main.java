e89d82e01a92c387e8c38ce450d871abf652e66e
kosperskij234 <kosperskij234@gmail.com>
2020-06-28 13:04:49 +0300

    Hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello\nWorld!") ;
+    }
+    int[]={1,2,3,4,5}
+    for(int i=0; i< 10; i++)
+    {
+        for (int j = 0; j < 15; j++){
+            System.out.print(10[i][j]);
+    }
